af94e0d831f41635380d679ee6e63f63b757f196
Quynh Nguyen <mr.quynh@gmail.com>
2018-12-07 22:01:55 +0700

    Refactoring: rename directory and update docs

---
+++ README.md
@@ -0,0 +1,6 @@
+# Services.Users
+
+Provide services for working with users in the VIEApps NGX
+
+- Built-in users, OAuth users (like Facebook, Google, Microsoft, Twitter, GitHub, ...) and Windows AD user
+- Verify and change password of Windows AD users
--- Service.WindowsAD/Main.cs
+++ Service.WindowsAD/Main.cs
@@ -14,11 +14,9 @@ namespace net.vieapps.Services.Users.WindowsAD
 {
 	public class ServiceComponent : ServiceBase
 	{
-		public ServiceComponent() { }
+		public override string ServiceName => "WindowsAD";
 
 		public override void Start(string[] args = null, bool initializeRepository = true, Func<IService, Task> nextAsync = null) => base.Start(args, false, nextAsync);
-
-		public override string ServiceName => "WindowsAD";
 
 		public override async Task<JToken> ProcessRequestAsync(RequestInfo requestInfo, CancellationToken cancellationToken = default(CancellationToken))
 		{
@@ -28,6 +26,9 @@ namespace net.vieapps.Services.Users.WindowsAD
 				{
 					case "POST":
 						return await UtilityService.ExecuteTask(() => this.SignIn(requestInfo), cancellationToken).ConfigureAwait(false);
+
+					case "PUT":
+						return await UtilityService.ExecuteTask(() => this.ChangePassword(requestInfo), cancellationToken).ConfigureAwait(false);
 
 					default:
 						throw new InvalidRequestException($"The request is invalid [({requestInfo.Verb}): {requestInfo.GetURI()}]");
@@ -55,6 +56,35 @@ namespace net.vieapps.Services.Users.WindowsAD
 			using (var context = new PrincipalContext(domain.PositionOf(".") < 0 ? ContextType.Machine : ContextType.Domain, domain))
 			{
 				if (!context.ValidateCredentials(username, password, ContextOptions.Negotiate))
+					throw new WrongAccountException();
+			}
+
+			return new JObject();
+		}
+
+		JObject ChangePassword(RequestInfo requestInfo)
+		{
+			// verify
+			if (requestInfo.Extra == null || !requestInfo.Extra.ContainsKey("Signature") || !requestInfo.Extra["Signature"].Equals(requestInfo.Body.GetHMACSHA256(this.ValidationKey)))
+				throw new InformationInvalidException();
+
+			// prepare
+			var body = requestInfo.GetBodyExpando();
+			var domain = body.Get<string>("Domain").Decrypt(this.EncryptionKey).Trim();
+			var username = body.Get<string>("Username").Decrypt(this.EncryptionKey).Trim();
+			var password = body.Get<string>("Password").Decrypt(this.EncryptionKey);
+			var oldPassword = body.Get<string>("OldPassword").Decrypt(this.EncryptionKey);
+
+			// perform sign-in and change password with Windows Directory Service (when domain doesn't got dot (.) in the name, means local machine)
+			using (var context = new PrincipalContext(domain.PositionOf(".") < 0 ? ContextType.Machine : ContextType.Domain, domain))
+			{
+				if (context.ValidateCredentials(username, oldPassword, ContextOptions.Negotiate))
+					using (var user = System.DirectoryServices.AccountManagement.UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username))
+					{
+						user.ChangePassword(oldPassword, password);
+						user.Save();
+					}
+				else
 					throw new WrongAccountException();
 			}
 
